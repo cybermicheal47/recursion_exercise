abab908fcfb325e9e717704949347e2036606f26
cybermicheal47 <65826184+cybermicheal47@users.noreply.github.com>
2023-06-15 04:07:45 -0400

    Create merge.js

---
+++ merge.js
@@ -0,0 +1,53 @@
+function mergeSort(array) {
+  // Base case: if the array has 0 or 1 element, it is already sorted
+  if (array.length <= 1) {
+    return array;
+  }
+
+  // Divide the array into two halves
+  const middle = Math.floor(array.length / 2);
+  const leftHalf = array.slice(0, middle);
+  const rightHalf = array.slice(middle);
+
+  // Recursively sort the left and right halves
+  const sortedLeftHalf = mergeSort(leftHalf);
+  const sortedRightHalf = mergeSort(rightHalf);
+
+  // Merge the sorted halves
+  return merge(sortedLeftHalf, sortedRightHalf);
+}
+
+// Merge two sorted arrays into a single sorted array
+function merge(leftArray, rightArray) {
+  const mergedArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+    if (leftArray[leftIndex] <= rightArray[rightIndex]) {
+      mergedArray.push(leftArray[leftIndex]);
+      leftIndex++;
+    } else {
+      mergedArray.push(rightArray[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  // Add the remaining elements from the left or right array, if any
+  while (leftIndex < leftArray.length) {
+    mergedArray.push(leftArray[leftIndex]);
+    leftIndex++;
+  }
+
+  while (rightIndex < rightArray.length) {
+    mergedArray.push(rightArray[rightIndex]);
+    rightIndex++;
+  }
+
+  return mergedArray;
+}
+
+// Example usage:
+const unsortedArray = [5, 3, 8, 2, 1, 4];
+const sortedArray = mergeSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 8]
